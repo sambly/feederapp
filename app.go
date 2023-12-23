ed98ce2103532117e3cf42c492c431be806b0e57
Kudrjavcev.EA <sambly>
2023-12-23 14:45:24 +0300

    add func

--- app.go
+++ app.go
@@ -64,35 +64,6 @@ func (app *Application) onTrade(trade model.Trade) {
 			c.ActiveBuyQuoteVolume += trade.Price * trade.Quantity
 		}
 
-		// structContent := []model.Trade{}
-		// content, _ := os.ReadFile("trade.json")
-		// err := json.Unmarshal(content, &structContent)
-		// if err != nil {
-		// 	fmt.Println("trade.json")
-		// 	fmt.Println(err)
-		// }
-		// structContent = append(structContent, trade)
-		// file, _ := json.Marshal(structContent)
-		// err = os.WriteFile("trade.json", file, 0777)
-		// if err != nil {
-		// 	fmt.Println("trade.json")
-		// 	fmt.Println(err)
-		// }
-
-		// structContent1 := []model.Candle{}
-		// content, _ = os.ReadFile("c.json")
-		// err = json.Unmarshal(content, &structContent1)
-		// if err != nil {
-		// 	fmt.Println("c.json")
-		// 	fmt.Println(err)
-		// }
-		// structContent1 = append(structContent1, *c)
-		// file, _ = json.Marshal(structContent1)
-		// err = os.WriteFile("c.json", file, 0777)
-		// if err != nil {
-		// 	fmt.Println("c.json")
-		// 	fmt.Println(err)
-		// }
 	}
 
 }
@@ -118,23 +89,6 @@ func (app *Application) onCandle(candle model.Candle) {
 			c.QuoteVolume = candle.QuoteVolume
 			c.AmountTradeC = candle.AmountTradeC
 			c.Complete = candle.Complete
-
-			//app.dataFeed.TimeStartCandle = app.dataFeed.TimeStartCandle.Add(time.Minute)
-
-			// structContent := []model.Candle{}
-			// content, _ := os.ReadFile("candle.json")
-			// err := json.Unmarshal(content, &structContent)
-			// if err != nil {
-			// 	fmt.Println("candle.json")
-			// 	fmt.Println(err)
-			// }
-			// structContent = append(structContent, candle)
-			// file, _ := json.Marshal(structContent)
-			// err = os.WriteFile("candle.json", file, 0777)
-			// if err != nil {
-			// 	fmt.Println("candle.json")
-			// 	fmt.Println(err)
-			// }
 
 		}
 
---
+++ func.go
@@ -0,0 +1,46 @@
+package main
+
+// structContent := []model.Trade{}
+// content, _ := os.ReadFile("trade.json")
+// err := json.Unmarshal(content, &structContent)
+// if err != nil {
+// 	fmt.Println("trade.json")
+// 	fmt.Println(err)
+// }
+// structContent = append(structContent, trade)
+// file, _ := json.Marshal(structContent)
+// err = os.WriteFile("trade.json", file, 0777)
+// if err != nil {
+// 	fmt.Println("trade.json")
+// 	fmt.Println(err)
+// }
+
+// structContent1 := []model.Candle{}
+// content, _ = os.ReadFile("c.json")
+// err = json.Unmarshal(content, &structContent1)
+// if err != nil {
+// 	fmt.Println("c.json")
+// 	fmt.Println(err)
+// }
+// structContent1 = append(structContent1, *c)
+// file, _ = json.Marshal(structContent1)
+// err = os.WriteFile("c.json", file, 0777)
+// if err != nil {
+// 	fmt.Println("c.json")
+// 	fmt.Println(err)
+// }
+
+// structContent := []model.Candle{}
+// content, _ := os.ReadFile("candle.json")
+// err := json.Unmarshal(content, &structContent)
+// if err != nil {
+// 	fmt.Println("candle.json")
+// 	fmt.Println(err)
+// }
+// structContent = append(structContent, candle)
+// file, _ := json.Marshal(structContent)
+// err = os.WriteFile("candle.json", file, 0777)
+// if err != nil {
+// 	fmt.Println("candle.json")
+// 	fmt.Println(err)
+// }
