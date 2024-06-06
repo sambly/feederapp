5f6d2abeb30324f4768e6e17f4044af0f4a66edc
Kudrjavcev.EA <sambly>
2024-06-06 11:51:43 +0300

    edit file

--- gorutune/gorutune.go
+++
@@ -1,7 +0,0 @@
-package gorutune
-
-import "sync"
-
-// Глобальный атомарный счетчик горутин и карта имен горутин
-var GoroutineCounter int32
-var GoroutineNames sync.Map
--- main.go
+++ main.go
@@ -2,21 +2,17 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"sync/atomic"
 	"syscall"
 	"time"
 
 	"main/config"
 	"main/database"
 	"main/exchange"
-	"main/gorutune"
 	"main/logging"
 	"main/model"
-	"net/http"
 	_ "net/http/pprof"
 
 	"golang.org/x/sync/errgroup"
@@ -72,65 +68,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	srv := &http.Server{Addr: ":8080"}
-
 	g, gCtx := errgroup.WithContext(ctx)
-
-	g.Go(func() error {
-		logging.MyLogger.InfoLog.Println("Запуск HTTP-сервера на порту 8080")
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			return err
-		}
-		return nil
-	})
 
 	g.Go(func() error {
 		return app.Run(gCtx)
 	})
-
-	// Завершение работы сервиса
-	g.Go(func() error {
-
-		<-gCtx.Done()
-
-		logging.MyLogger.InfoLog.Println("Завершение приложения")
-
-		ctxShutdown, cancelShutdown := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancelShutdown()
-		if err := srv.Shutdown(ctxShutdown); err != nil {
-			logging.MyLogger.ErrorOut(fmt.Errorf("ошибка при завершении работы HTTP-сервера: %v", err))
-		}
-		logging.MyLogger.InfoLog.Println("HTTP-сервер остановлен")
-		return gCtx.Err()
-	})
-
-	// Запуск горутины для мониторинга количества горутин
-	go func() {
-		ticker := time.NewTicker(1 * time.Second)
-		defer ticker.Stop()
-
-		for {
-			select {
-			case <-ticker.C:
-				fmt.Printf("Number of goroutines: %d\n", atomic.LoadInt32(&gorutune.GoroutineCounter))
-
-			case <-gCtx.Done():
-				return
-			}
-		}
-	}()
 
 	if err := g.Wait(); err != nil && gCtx.Err() != context.Canceled {
 		log.Fatalf("Приложение завершено с ошибкой: %v", err)
 	} else {
 		logging.MyLogger.InfoLog.Println("Приложение завершено")
 	}
-
-	fmt.Printf("Finished Number of goroutines: %d\n", atomic.LoadInt32(&gorutune.GoroutineCounter))
-
-	gorutune.GoroutineNames.Range(func(key, value interface{}) bool {
-		fmt.Printf("Remaining goroutine ID %d: %s\n", key, value)
-		return true
-	})
 
 }
